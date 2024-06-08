628befa2febb05289c50287aaddf7083cc21c737
sangamkunwar <sangamkunwar48@gmail.com>
2024-06-08 21:56:22 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings: ["Frontend Development", "Youtuber", "Web Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
